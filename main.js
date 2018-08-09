562a413547011856f0d139dc891b4eae43ca9d8d
xiaolong3000 <276947850@qq.com>
2018-08-09 11:57:09 +0800

    ?

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
-const ipcMain=app.ipcMain;
+//const ipcMain=app.ipcMain;
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
@@ -11,6 +12,7 @@ function createWindow () {
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`)
+  
 //    mainWindow.loadURL('http://www.zhangpeilong.com')
   // Open the DevTools.
    mainWindow.webContents.openDevTools()
