db829ce5b9cd511aa6a54d142396fec41b083035
xiaolong3000 <276947850@qq.com>
2019-05-25 01:59:16 -0700

    start

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
-//const ipcMain=app.ipcMain;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -8,14 +7,19 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
 
   // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`)
-  
-//    mainWindow.loadURL('http://www.zhangpeilong.com')
+  mainWindow.loadFile('index.html')
+
   // Open the DevTools.
-   mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
@@ -24,32 +28,24 @@ function createWindow () {
     // when you should delete the corresponding element.
     mainWindow = null
   })
-  //message from renderer.js
-  // mainWindow.webContents.on('did-finish-load', () => {
-  //   mainWindow.webContents.send('main-process-messages', 'webContents event "did-finish-load" called');
-  // });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready',  createWindow)
+app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
+  // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+  if (process.platform !== 'darwin') app.quit()
 })
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
+  // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
+  if (mainWindow === null) createWindow()
 })
 
 // In this file you can include the rest of your app's specific main process
--- renderer.js
+++ renderer.js
@@ -1,11 +1,3 @@
-
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-const electron = require('electron');
-//const ipcRenderer = electron.ipcRenderer;
-// 设置监听
-// ipcRenderer.on('main-process-messages', (event, message) => {
-//   console.log('message from Main Process: ' , message);  // Prints Main Process Message.
-// });
-// ipcRenderer.sendSync('synchronous-message', 'hello');
